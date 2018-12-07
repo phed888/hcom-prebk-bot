390d2b9651566d2919160d77996b4787327ce190
Brian Speight <brspeight@expedia.com>
2018-12-06 16:52:51 -0800

    Added provision to have the last chat visible even when it goes below the fold

--- src/components/C3Body.js
+++ src/components/C3Body.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import C3Chat from './C3Chat';
+import C3HeaderHcom from './C3HeaderHcom';
 
 class C3Body extends Component {
+  scrollToBottom() {
+    document
+      .getElementById('scrollTarget')
+      .scrollIntoView({ behavior: 'smooth' });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
-    const spreadList = this.props.conversation.map(message => (
-      <li className="messageBubble">
+    const spreadList = this.props.conversation.map((message, index) => (
+      <li className="messageBubble" key={index}>
         <C3Chat
           avatar=""
           agent={message.agent}
@@ -14,9 +29,12 @@ class C3Body extends Component {
         />
       </li>
     ));
+
     return (
       <div className="c3body">
+        <C3HeaderHcom />
         <ul className="chat-messages">{spreadList}</ul>
+        <div id="scrollTarget">&nbsp;</div>
       </div>
     );
   }
